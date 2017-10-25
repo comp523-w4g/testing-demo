5864faf00c22737211e19e9f16eb0f0a74d6c70b
Victor Cui <victor.cui1996@gmail.com>
2017-10-25 14:46:43 -0400

    Add me and amoghs name.

--- .deploy.sh
+++ .deploy.sh
@@ -18,4 +18,4 @@ git add .
 git commit -m ":shipit: Deploy to GitHub Pages"
 
 # Force push to gh-pages
-git push --force "https://${GH_TOKEN}@github.com/victorcui96/testing-demo.git" master:gh-pages > /dev/null 2>&1
+git push --force "https://${GH_TOKEN}@github.com/comp523-w4g/testing-demo.git" master:gh-pages > /dev/null 2>&1
